2e4e82097a1d8b3fe89216d3da01625ecae4937b
Scott Endicott <sendicott1391@gmail.com>
2016-08-23 20:24:24 -0400

    Some SASS and HTML structure added

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-// Build a weather app that displays 5-day forecast for a given place
+// Build a weather app that displays a 5-day forecast for a given place
 // and allows users to provide a place name and select between
 // Fahrenheit and Celsius
 
@@ -13,13 +13,16 @@
  *  6. Optional: air pressure, etc.
  */
 
-function weekdayForecast() {
+
+// This needs to receive two inputs for city and state that will then 
+// go into the url and GET the weather information for that location
+function weekdayForecast(city, stateAbbr) {
+    let url = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + stateAbbr + '&appid=1f2671239fb2a0b6556a93f5873da5b1';
     let request = new XMLHttpRequest();
-    request.open('GET', 'http://api.openweathermap.org/data/2.5/forecast?q=Charlotte,NC&appid=1f2671239fb2a0b6556a93f5873da5b1');
+    request.open('GET', url);
     request.addEventListener('load', function() {
         let response = JSON.parse(request.responseText);
         console.log(response);
-        console.log(request);
     });
     request.send();
 }
@@ -27,7 +30,7 @@ function weekdayForecast() {
 
 
 window.addEventListener('load', function() {
-    weekdayForecast();
+    let userLocation = prompt("Add a city and state with a comma inbetween and no spaces");
+    weekdayForecast(userLocation);
 });
 
-// testing testing
